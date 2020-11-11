c4d96b0e558805ba37d916fc5910d15da80b3894
anchiseong <anchiseong@localhost>
2020-11-11 15:40:11 +0900

    오픈 API(구 Swagger) 그룹핑 수정.

--- src/main/java/com/scglab/connect/base/config/OpenApiConfig.java
+++ src/main/java/com/scglab/connect/base/config/OpenApiConfig.java
@@ -26,7 +26,7 @@ public class OpenApiConfig {
 	   return new OpenAPI()
 	          .components(new Components()
 	          .addSecuritySchemes("bearer-key", 
-	          new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT").description("로그인을 통해 발급된 인증토큰을 삽입한다.<br>eyJhbGciOiJIUzI1NiJ9.eyJhdXRoIjoyLCJzcGVha2VyIjoxNzcsIm5hbWUiOiLshJzsmrjrj4Tsi5zqsIDsiqQiLCJlbXAiOjEsImVtcG5vIjoiY3NtYXN0ZXIxIiwiZXhwIjoxNjAzODQ1MTgzLCJpYXQiOjE2MDM3NTg3ODMsImNpZCI6MX0.X8BsWZ3g9e3YI4_PukdGyFWVx_YLNcydQhLzbeDT1V8")));
+	          new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT").description("로그인을 통해 발급된 인증토큰을 삽입한다.<br>eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NzAsImNvbXBhbnlJZCI6IjIiLCJjb21wYW55VXNlQ29uZmlnSnNvbiI6bnVsbCwiY29tcGFueU5hbWUiOiLsnbjsspzrj4Tsi5zqsIDsiqQiLCJpc0FkbWluIjoxLCJhdXRoTGV2ZWwiOjIsImxvZ2luTmFtZSI6bnVsbCwic3RhdGUiOjAsInByb2ZpbGVJbWFnZUlkIjowLCJzcGVha2VySWQiOjIxOSwibmFtZSI6IuyViOy5mOyEsSIsImlhdCI6MTYwNDk5MjM4NiwiZXhwIjoxNjA1MDc4Nzg2fQ.Da4hSzwLSbqb9wiTtZ5Hfce6m8bbLuEYXU2MzcdmiuE")));
 	}
 	
 	@Bean
@@ -41,6 +41,7 @@ public class OpenApiConfig {
 		return GroupedOpenApi.builder().setGroup("인증 API").pathsToMatch(paths).build();
 	}
 	
+	/*
 	@Bean
 	public GroupedOpenApi customer() {
 		String[] paths = { "/customers/**" };
@@ -63,5 +64,60 @@ public class OpenApiConfig {
 	public GroupedOpenApi talkMenu() {
 		String[] paths = { "/talk/**" };
 		return GroupedOpenApi.builder().setGroup("상담채팅 API").pathsToMatch(paths).build();
+	}
+	*/
+	
+	@Bean
+	public GroupedOpenApi company() {
+		String[] paths = { "/api/company/**" };
+		return GroupedOpenApi.builder().setGroup("회사관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi member() {
+		String[] paths = { "/api/member/**" };
+		return GroupedOpenApi.builder().setGroup("회원관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi room() {
+		String[] paths = { "/api/room/**" };
+		return GroupedOpenApi.builder().setGroup("상담(채팅방)관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi minwon() {
+		String[] paths = { "/api/minwon/**" };
+		return GroupedOpenApi.builder().setGroup("민원관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi category() {
+		String[] paths = { "/api/category/**" };
+		return GroupedOpenApi.builder().setGroup("카테고리관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi template() {
+		String[] paths = { "/api/template/**" };
+		return GroupedOpenApi.builder().setGroup("템플릿관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi manual() {
+		String[] paths = { "/api/manual/**" };
+		return GroupedOpenApi.builder().setGroup("메뉴얼 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi customer() {
+		String[] paths = { "/api/customer/**" };
+		return GroupedOpenApi.builder().setGroup("고객관리 API").pathsToMatch(paths).build();
+	}
+	
+	@Bean
+	public GroupedOpenApi stats() {
+		String[] paths = { "/api/stats/**" };
+		return GroupedOpenApi.builder().setGroup("통계관리 API").pathsToMatch(paths).build();
 	}
 }
