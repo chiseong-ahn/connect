e31885b9771ce717e2314e9ff3267c9ba6a53f0d
anchiseong <anchiseong@localhost>
2020-11-16 16:58:27 +0900

    채팅메세지 API 기능 구현.

---
+++ src/main/java/com/scglab/connect/services/message/Message.java
@@ -0,0 +1,15 @@
+package com.scglab.connect.services.message;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Schema(description = "메세지 정보")
+@Setter
+@Getter
+@ToString
+public class Message {
+	private Long id;					// 방id
+	
+}
