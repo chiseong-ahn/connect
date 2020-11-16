d7862d5a45f841f1010ccad04e25026f87c66499
anchiseong <anchiseong@localhost>
2020-11-16 16:56:38 +0900

    자동메세지 API 기능 개발.

---
+++ src/main/java/com/scglab/connect/services/automessage/AutoMessageController.java
@@ -0,0 +1,83 @@
+package com.scglab.connect.services.automessage;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scglab.connect.base.annotations.Auth;
+import com.scglab.connect.constant.Constant;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auto-message")
+@Tag(name = "자동메세지 관리", description = "자동메세지 API를 제공합니다.")
+public class AutoMessageController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private AutoMessageService autoMessageService;
+	
+	@Auth
+	@RequestMapping(method = RequestMethod.GET, value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary="자동메세지 목록", description = "자동메세지 목록", security = {@SecurityRequirement(name = Constant.AUTH_BEARERR_KEY)})
+	public List<AutoMessage> findAll(@Parameter(hidden = true) @RequestParam Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return this.autoMessageService.findAll(params, request, response);
+	}
+	
+	
+	@Auth
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary="자동메세지 등록", description = "자동메세지 상세", security = {@SecurityRequirement(name = Constant.AUTH_BEARERR_KEY)})
+	public AutoMessage detail(@Parameter(description = "자동메세지 id") @PathVariable int id, @Parameter(hidden = true) @RequestParam Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		params.put("id", id);
+		return this.autoMessageService.getDetail(params, request, response);
+	}
+	
+	
+	@Auth
+	@RequestMapping(method = RequestMethod.POST, value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary="자동메세지 등록", description = "자동메세지 등록", security = {@SecurityRequirement(name = Constant.AUTH_BEARERR_KEY)})
+	public AutoMessage regist(@Parameter(hidden = true) @RequestParam Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return this.autoMessageService.regist(params, request, response);
+	}
+	
+	
+	@Auth
+	@RequestMapping(method = RequestMethod.PUT, value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary="자동메세지 수정", description = "자동메세지 수정", security = {@SecurityRequirement(name = Constant.AUTH_BEARERR_KEY)})
+	public AutoMessage update(@Parameter(description = "자동메세지 id") @PathVariable int id, @Parameter(hidden = true) @RequestParam Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		params.put("id", id);
+		return this.autoMessageService.update(params, request, response);
+	}
+	
+	
+	@Auth
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary="자동메세지 삭제", description = "자동메세지 삭제", security = {@SecurityRequirement(name = Constant.AUTH_BEARERR_KEY)})
+	public Map<String, Object> delete(@Parameter(description = "자동메세지 id") @PathVariable int id, @Parameter(hidden = true) @RequestParam Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		params.put("id", id);
+		return this.autoMessageService.delete(params, request, response);
+	}
+	
+	
+}
+	
---
+++ src/main/java/com/scglab/connect/services/automessage/AutoMessageService.java
@@ -0,0 +1,171 @@
+package com.scglab.connect.services.automessage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.scglab.connect.services.common.service.MessageHandler;
+import com.scglab.connect.utils.DataUtils;
+
+@Service
+public class AutoMessageService {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private MessageHandler messageService;
+	
+	@Autowired
+	private AutoMessageDao autoMessageDao;
+	
+	/**
+	 * 
+	 * @Method Name : findAll
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 자동메세지 목록 조회.
+	 * @param params 
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public List<AutoMessage> findAll(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		return this.autoMessageDao.findAutoMessageAll(params); 
+	}
+	
+	/**
+	 * 
+	 * @Method Name : getDetail
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 자동메세지 상세 조회.
+	 * @param params
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public AutoMessage getDetail(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		return this.autoMessageDao.getAutoMessage(params);
+	}
+	
+	/**
+	 * 
+	 * @Method Name : getAutoMessageWelcome
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 신규대화 시작시 인사메세지 랜덤 조회
+	 * @param params
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public AutoMessage getAutoMessageWelcome(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		return this.autoMessageDao.getAutoMessageWelcome(params);
+	}
+	
+	/**
+	 * 
+	 * @Method Name : getAutoMessageByMatchWait
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 상담사 배정지연 안내 메세지 랜덤 조회.
+	 * @param params
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public AutoMessage getAutoMessageByMatchWait(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		return this.autoMessageDao.getAutoMessageByMatchWait(params);
+	}
+	
+	/**
+	 * 
+	 * @Method Name : getAutoMessageByReplyWait
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 답변 지연 안내 메세지 랜덤 조회.
+	 * @param params
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public AutoMessage getAutoMessageByReplyWait(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		return this.autoMessageDao.getAutoMessageByReplyWait(params);
+	}
+	
+	
+	/**
+	 * 
+	 * @Method Name : regist
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 자동 메세지 등록.
+	 * @param params
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public AutoMessage regist(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		AutoMessage autoMessage = null;
+		int result = this.autoMessageDao.createAutoMessage(params);
+		if(result > 0) {
+			autoMessage = this.autoMessageDao.getAutoMessage(params);
+		}
+		return autoMessage;
+	}
+	
+	/**
+	 * 
+	 * @Method Name : update
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 자동 메세지 수정.
+	 * @param params 
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public AutoMessage update(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		AutoMessage autoMessage = null;
+		int result = this.autoMessageDao.updateAutoMessage(params);
+		if(result > 0) {
+			autoMessage = this.autoMessageDao.getAutoMessage(params);
+		}
+		return autoMessage;
+	}
+	
+	/**
+	 * 
+	 * @Method Name : delete
+	 * @작성일 : 2020. 11. 16.
+	 * @작성자 : anchiseong
+	 * @변경이력 : 
+	 * @Method 설명 : 자동 메세지 삭제.
+	 * @param params
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public Map<String, Object> delete(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> data = new HashMap<String, Object>();
+		int result = this.autoMessageDao.deleteAutoMessage(params);
+		data.put("success", result > 0 ? true : false);
+		return data;
+	}
+	
+	
+}
