25d26ed897097e2ed7332fa5c2b686b1938e7d24
yamdeng <yamdeng@gmail.com>
2021-01-04 19:36:32 +0900

    직원상세 api url 변경, 로그인시 회원이름정보 가져오기, 민원등록시 자료형 에러

--- src/main/java/com/scglab/connect/services/minwon/MinwonService.java
+++ src/main/java/com/scglab/connect/services/minwon/MinwonService.java
@@ -24,28 +24,28 @@ import com.scglab.connect.utils.DataUtils;
 @Service
 public class MinwonService {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired private MessageHandler messageService;
 	@Autowired private MinwonDao minwonDao;
 	@Autowired private LoginService loginService;
 	@Autowired private CommonService commonService;
 	@Autowired private ErrorService errorService;
-	
+
 	public List<Map<String, Object>> codes(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		Member member = this.loginService.getMember(request);
 		params.put("companyId", member.getCompanyId());
 		params.put("loginId", member.getId());
-		
+
 		// 각 회사별 기간망 클래스 가져오기.
 		ICompany company = this.commonService.getCompany(member.getCompanyId());
-		
+
 		List<Map<String, Object>> codes = company.getMinwonsCodes();
-		
+
 		return codes == null ? new ArrayList<>() : codes;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @Method Name : regist
 	 * @작성일 : 2020. 11. 12.
 	 * @작성자 : anchiseong
@@ -60,34 +60,34 @@ public class MinwonService {
 	public Minwon regist(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		Member member = this.loginService.getMember(request);
 		params.put("companyId", member.getCompanyId());
-		params.put("loginId", member.getId());		
-		
+		params.put("loginId", member.getId());
+
 		String errorParams = "";
-	    if(!this.commonService.valid(params, "gasappMemberNumber"))
-	        errorParams = this.commonService.appendText(errorParams, "가스앱 회원번호-gasappMemberNumber");
-	    if(!this.commonService.valid(params, "useContractNum"))
-	        errorParams = this.commonService.appendText(errorParams, "사용계약번호-useContractNum");
-	    if(!this.commonService.valid(params, "categorySmallId"))
-	        errorParams = this.commonService.appendText(errorParams, "소분류 카테고리id-categorySmallId");
-	    if(!this.commonService.valid(params, "minwonCode"))
-	        errorParams = this.commonService.appendText(errorParams, "민원코드-minwonCode");
-	    if(!this.commonService.valid(params, "telNumber"))
-	        errorParams = this.commonService.appendText(errorParams, "휴대폰번호-telNumber");
-	    if(!this.commonService.valid(params, "chatId"))
-	        errorParams = this.commonService.appendText(errorParams, "룸에 대한 chatId-chatId");
-	    if(!this.commonService.valid(params, "roomId"))
-	        errorParams = this.commonService.appendText(errorParams, "방id-roomId");
-	    
-	    // 파라미터 유효성 검증.
-	    if(!errorParams.equals("")) {
-	        // 필수파라미터 누락에 따른 오류 유발처리.
-	        this.errorService.throwParameterErrorWithNames(errorParams);
-	    }
-		
+		if(!this.commonService.valid(params, "gasappMemberNumber"))
+			errorParams = this.commonService.appendText(errorParams, "가스앱 회원번호-gasappMemberNumber");
+		if(!this.commonService.valid(params, "useContractNum"))
+			errorParams = this.commonService.appendText(errorParams, "사용계약번호-useContractNum");
+		if(!this.commonService.valid(params, "categorySmallId"))
+			errorParams = this.commonService.appendText(errorParams, "소분류 카테고리id-categorySmallId");
+		if(!this.commonService.valid(params, "minwonCode"))
+			errorParams = this.commonService.appendText(errorParams, "민원코드-minwonCode");
+		if(!this.commonService.valid(params, "telNumber"))
+			errorParams = this.commonService.appendText(errorParams, "휴대폰번호-telNumber");
+		if(!this.commonService.valid(params, "chatId"))
+			errorParams = this.commonService.appendText(errorParams, "룸에 대한 chatId-chatId");
+		if(!this.commonService.valid(params, "roomId"))
+			errorParams = this.commonService.appendText(errorParams, "방id-roomId");
+
+		// 파라미터 유효성 검증.
+		if(!errorParams.equals("")) {
+			// 필수파라미터 누락에 따른 오류 유발처리.
+			this.errorService.throwParameterErrorWithNames(errorParams);
+		}
+
 		Minwon minwon = null;
 		if(this.minwonDao.insertMinwon(params) > 0) {
 			minwon = this.minwonDao.findMinwon(params);
-			
+
 			// 각 회사별 기간망 클래스 가져오기.
 			ICompany company = this.commonService.getCompany(member.getCompanyId());
 			
@@ -111,12 +111,12 @@ public class MinwonService {
 			obj.put("handphone", DataUtils.getString(params, "telNumber", ""));
 			obj.put("memo", DataUtils.getString(params, "memo", ""));
 			obj.put("employeeId", member.getId() + "");
-			obj.put("chatId", DataUtils.getString(params, "chatId", ""));
+			obj.put("chatId", Integer.toString(DataUtils.getInt(params, "chatId", 0)));
 			this.logger.info("Minwon > " + obj.toString());
-			
+
 			company.minwons(obj);
 		}
 		return minwon;
 	}
-	
+
 }
