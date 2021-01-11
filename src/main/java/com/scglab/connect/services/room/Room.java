089d2d73420bb7b74cb1f25c9e8caa7f6dc8a52f
anchiseong <anchiseong@localhost>
2021-01-11 13:18:55 +0900

    방 정보에 고객의 관심고객 정보를 포함하여 전달하도록 변경.

--- src/main/java/com/scglab/connect/services/room/Room.java
+++ src/main/java/com/scglab/connect/services/room/Room.java
@@ -39,6 +39,8 @@ public class Room {
 	private String gasappMemberNumber;	// 고객의 가스앱 모바일 id
 	private String telNumber;			// 고객 전화번호
 	private int isBlockCustomer;		// 고객의 block 상태
+	private String blockType;			// block 유형
+	private String remark;				// block memo
 	
 	private int speakMinute;			// 상담 시간
 	private int joinHistoryCount;		// 이전 상담대화 건 수.
