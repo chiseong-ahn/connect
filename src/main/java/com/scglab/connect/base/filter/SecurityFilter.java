aff59ed98d9c072890f179487409a085c0e36602
csahn <csahn@scglab.com>
2021-07-09 16:43:29 +0900

    SBA에 대한 로그 미출력 처리.

--- src/main/java/com/scglab/connect/base/filter/SecurityFilter.java
+++ src/main/java/com/scglab/connect/base/filter/SecurityFilter.java
@@ -9,31 +9,39 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SecurityFilter implements Filter {
-
+	
+	private final String endpointBasePath = "/api/actuator/tomcat";
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		
-		LocalTime startTime = LocalTime.now();
-		logger.debug("프로세스 시작. : " + startTime);
+		final String uri = ((HttpServletRequest)req).getRequestURI();
 		
-		chain.doFilter(req, res);
 		
-		LocalTime endTime = LocalTime.now();
-		logger.debug("프로세스 종료. : " + endTime);
-
-		
-		Duration duration = Duration.between(startTime, endTime);
-
-		//long diffSeconds = duration.getSeconds();
-		long diffMillis = duration.toMillis();
-		logger.debug("프로세스 처리시간 : " + diffMillis + "ms");
+		if (StringUtils.contains(uri, endpointBasePath)) {
+			chain.doFilter(req, res);
+		}else {
+			LocalTime startTime = LocalTime.now();
+			logger.debug("프로세스 시작. : " + startTime);
+			
+			chain.doFilter(req, res);
+			
+			LocalTime endTime = LocalTime.now();
+			logger.debug("프로세스 종료. : " + endTime);
+	
+			Duration duration = Duration.between(startTime, endTime);
+	
+			long diffMillis = duration.toMillis();
+			logger.debug("프로세스 처리시간 : " + diffMillis + "ms");
+		}
 	}
 
 }
